76912e02c3ef02611338d4e932fc427badcaf477
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-11-02 21:11:17 +0530

    Fast API

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import uvicorn
 
 
 if __name__ == "__main":
-    uvicorn.run(app "app.app:app", host="0.0.0.0", port=8000, reload=true)
+    uvicorn.run(app: "app.app:app", host="0.0.0.0", port=8000, reload=true)
